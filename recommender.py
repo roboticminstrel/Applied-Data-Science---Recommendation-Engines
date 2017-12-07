cda8b84c4bb9032421eebb740ee682b72ea9bddf
c e smith <crazyfox@cs-MacBook.local>
2017-12-07 14:52:53 -0800

    add estimate_rating_prediction. Note that for users not already in the train_data, they should be added to train_data, rerun user_similarity, rerun derive_prediction_matrix.

--- recommender.py
+++ recommender.py
@@ -19,15 +19,10 @@ user_similarity = pairwise_distances(train_data_ptable, metric='cosine')
 item_similarity = pairwise_distances(train_data_ptable.T, metric='cosine')
 
 # PREDICTION MATRIX
-''' 
-MATHJAX
-This is the formula applied below in derive_prediction_matrix()
-\[ \hat{x}_{k,m} = \bar{x}_{k} + \frac{\sum\limits_{u_a} sim_u(u_k, u_a) (x_{a,m} - \bar{x}_{u_a})}{\sum\limits_{u_a}|sim_u(u_k, u_a)|} \]
-'''
 def derive_prediction_matrix(ratings_array, similarity, type='user'):
     if type == 'user':
-        # Normalize user ratings
         mean_user_rating = ratings_array.mean(axis=1)
+        # subtract mean rating from each rating
         ratings_diff = (ratings_array - mean_user_rating[:, numpy.newaxis])
         # Generate predictions
         pred = mean_user_rating[:, numpy.newaxis] + similarity.dot(ratings_diff) / numpy.array([numpy.abs(similarity).sum(axis=1)]).T
@@ -37,33 +32,32 @@ def derive_prediction_matrix(ratings_array, similarity, type='user'):
     return pred
 
 user_prediction_matrix = derive_prediction_matrix(numpy.array(train_data_ptable), user_similarity, type='user')
+
 # game_rating_array = numpy.array(train_data_ptable.T)
 #item_prediction_matrix = derive_prediction_matrix(game_rating_array, item_similarity, type='item')
 
 #*********************************************************************************************
 # PREDICT rating GIVEN userID AND gameID
-# Working... Needs to account for user not in DB but has a rating array?
+# Users not already in train_data_ptable need to be added and run through derive_prediction_matrix
+def estimate_rating_prediction(userID, gameID):
+    for i, user in enumerate(train_data_ptable.index.values):
+        if user == userID:
+            for j, game in enumerate(train_data_ptable.columns):
+                if game == gameID:
+                    return user_prediction_matrix[i][j]
 
-def rating_prediction(userID, gameID):
-    # Ensure userID has not already rated game
-    assert(all_data_ptable[userID][gameID] == 0)
-    # create an array from userID
-    userRatingArray = numpy.array(all_data_ptable[userID])
-    gameRatingArray = numpy.array(all_data_ptable.T[gameID])
-    
-rating_prediction(3, 5038)
-
-
+print("userID 272 would rate gameID 118 at: ", estimate_rating_prediction(272, 118))
 #**********************************************************************************************
 # EVALUATION, RMSE
 
-# from sklearn.metrics import mean_squared_error
-# from math import sqrt
-# def rmse(prediction, ground_truth):
-#     prediction = prediction[ground_truth.nonzero()].flatten()
-#     ground_truth = ground_truth[ground_truth.nonzero()].flatten()
-#     return sqrt(mean_squared_error(prediction, ground_truth))
-# 
-# test_data_array = numpy.array(test_data_ptable)
-# print('User-based CF RMSE: ' + str(rmse(user_prediction_matrix, test_data_array)))
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+def rmse(prediction, ground_truth):
+    prediction = prediction[ground_truth.nonzero()].flatten()
+    ground_truth = ground_truth[ground_truth.nonzero()].flatten()
+    return sqrt(mean_squared_error(prediction, ground_truth))
+
+test_data_array = numpy.array(test_data_ptable)
+print('User-based CF RMSE: ' + str(rmse(user_prediction_matrix, test_data_array)))
 # print('Item-based CF RMSE: ' + str(rmse(item_prediction_matrix, test_data_array)))
+
